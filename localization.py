13afc73ef5de76a6f0835ae62cea97c74db74014
Unknown <umbertofazio92@gmail.com>
2018-03-13 20:30:57 +0100

    udpy: write method; localization: compute dist

--- localization.py
+++ localization.py
@@ -1,3 +1,19 @@
 #Localization
 
 #dev
+import udpy
+import ast
+import numpy
+
+udp_dao = udpy.UDP_DAO('127.0.0.1', 12346)
+
+while True:
+    data = udp_dao.readData()
+    # convert in dictionary
+    data_dic = ast.literal_eval(data)
+    if data_dic['major'] == 1 and (data_dic['minor'] == 1 or data_dic['minor'] == 2 or data_dic['minor'] == 3 or data_dic['minor'] == 4 or data_dic['minor'] == 5):
+        #conpute distance from the beacon
+        dist = 10.0 ** ((data_dic['measuredPower'] - data_dic['rssi'])/20.0)
+        x,y
+        x_hat, y_hat = kalman(x, y, state)
+        print  'minor: ' + str(data_dic['minor']) + ' - rssi: ' + str(data_dic['rssi']) + ' - dist: ' + str(dist)
--- udpy.py
+++ udpy.py
@@ -1,16 +1,19 @@
 import socket
-#import ast
 
+class UDP_DAO:
 
-class UDP_DAO(object):
-    """docstring for UDP_DAO."""
     def __init__(self, ip, port):
         self.UDP_IP = ip
         self.UDP_PORT = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind((self.UDP_IP, self.UDP_PORT))
-        print 'UDP_DAO: ' + self.UDP_IP + " - " + str(self.UDP_PORT)
+        print 'UDP_DAO: ' + self.UDP_IP + ":" + str(self.UDP_PORT)
 
-    def read_data(self):
-        data, addr = self.sock.recvfrom(1024)
+    def readData(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+        sock.bind((self.UDP_IP, self.UDP_PORT))
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         return data
+
+    def writeData(self,message):
+        MESSAGE = message
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+        sock.sendto(MESSAGE, (self.UDP_IP, self.UDP_PORT))

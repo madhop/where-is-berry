f9e1c25c9f625178a68510920105d742ef5ea4a7
Unknown <umbertofazio92@gmail.com>
2018-03-13 18:06:28 +0100

    Filter with timestamp

--- python_udp.py
+++ python_udp.py
@@ -2,7 +2,7 @@ import socket
 import ast
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 1234
+UDP_PORT = 12346
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
@@ -13,4 +13,4 @@ while True:
     # convert in dictionary
     data_dic = ast.literal_eval(data)
     if data_dic['major'] == 1 and (data_dic['minor'] == 1 or data_dic['minor'] == 2 or data_dic['minor'] == 3 or data_dic['minor'] == 4):
-        print  'rssi: ' + str(data_dic['rssi']) + ' - minor: ' + str(data_dic['minor'])
+        print  'rssi: ' + str(data_dic['rssi']) + ' - minor: ' + str(data_dic['minor']) + ' - timestamp: ' + str(data_dic['timestamp'])

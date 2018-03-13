8880f2c90b159201a93aa21ba25a5ccf19163bc4
Unknown <umbertofazio92@gmail.com>
2018-03-13 23:22:07 +0100

    get list of different distances

--- localization.py
+++ localization.py
@@ -1,19 +1,41 @@
-#Localization
-
-#dev
 import udpy
 import ast
-import numpy
+import numpy as np
+import time
 
 udp_dao = udpy.UDP_DAO('127.0.0.1', 12346)
 
+beacon1 = np.array([0 , 0])
+
+majors_avail = [1]
+minors_avail = [1,2,3,4,5]
+data_interval = 1000
+n_diff_anchors = 4
+
+def count_n_diff_anchors(distances):
+    minors = []
+    for d in distances:
+        if d['minor'] not in minors:
+            minors.append(d['minor'])
+    return len(minors)
+
+
+avg_time = 0
 while True:
-    data = udp_dao.readData()
-    # convert in dictionary
-    data_dic = ast.literal_eval(data)
-    if data_dic['major'] == 1 and (data_dic['minor'] == 1 or data_dic['minor'] == 2 or data_dic['minor'] == 3 or data_dic['minor'] == 4 or data_dic['minor'] == 5):
-        #conpute distance from the beacon
-        dist = 10.0 ** ((data_dic['measuredPower'] - data_dic['rssi'])/20.0)
-        x,y
-        x_hat, y_hat = kalman(x, y, state)
-        print  'minor: ' + str(data_dic['minor']) + ' - rssi: ' + str(data_dic['rssi']) + ' - dist: ' + str(dist)
+    distances = []
+    ts_milli = time.time() * 1000
+    data = ast.literal_eval(udp_dao.readData())
+    if data['major'] in majors_avail and (data['minor'] in minors_avail):
+        distances.append(data)
+        while (data['timestamp'] - ts_milli) < data_interval or count_n_diff_anchors(distances) < n_diff_anchors:
+            data = ast.literal_eval(udp_dao.readData())
+            if data['major'] in majors_avail and (data['minor'] in minors_avail):
+                distances.append(data)
+                #compute distance from the beacon
+                dist = 10.0 ** ((- data['rssi'] - (77) )/20.0)#dist = 10.0 ** (-(data['measuredPower'] - data['rssi'])/20.0)
+                print  'minor: ' + str(data['minor']) + ' - rssi: ' + str(data['rssi']) + ' - time: ' + str(data['timestamp']) + ' - dist: ' + str(dist)
+        print 'distances: ', len(distances)
+        end_time = time.time() * 1000
+        
+        #avg_time = ((end_time - start_time)/(i+1)) + avg_time*(i/(i+1))
+        #print 'time: ', end_time-ts_milli

0d946bbd84226bcd8efe3d09107613420c5e938a
Luca <luca.fucci@outlook.com>
2018-03-15 16:58:11 +0100

    Beacon class, measures.csv

---
+++ Scripts/beacon.py
@@ -0,0 +1,16 @@
+class Beacon:
+    def __init__(self,uuid,major,minor,x,y,z):
+        self.uuid = uuid
+        self.major = major
+        self.minor = minor
+        self.x = x
+        self.y = y
+        self.z = z
+        self.measures = []
+
+
+class Measure:
+    def __init__(self, rssi, distance, timestamp):
+        self.rssi = rssi
+        self.distance = distance
+        self.timestamp = timestamp

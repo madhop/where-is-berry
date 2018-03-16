08e688d93234e7aaf51e518eeb7c66231292d297
Unknown <umbertofazio92@gmail.com>
2018-03-16 18:42:32 +0100

    trilateration

--- Scripts/beacons_config.py
+++ Scripts/beacons_config.py
@@ -8,9 +8,9 @@ import beacon as b
 import pprint as pp
 input_list = [{'uuid': "b9407f30f5f8466eaff925556b57fe6d", 'major': 1, 'minor': 1, 'x':3, 'y': 3, 'z':0},
 {'uuid': "b9407f30f5f8466eaff925556b57fe6d", 'major': 1, 'minor': 4, 'x':3, 'y': 67, 'z':0},
-{'uuid': "b9407f30f5f8466eaff925556b57fe6d", 'major': 1, 'minor': 2, 'x':3, 'y': 67, 'z':0},
-{'uuid': "b9407f30f5f8466eaff925556b57fe6d", 'major': 1, 'minor': 3, 'x':3, 'y': 67, 'z':0},
-{'uuid': "b9407f30f5f8466eaff925556b57fe6d", 'major': 1, 'minor': 5, 'x':3, 'y': 67, 'z':0}]
+{'uuid': "b9407f30f5f8466eaff925556b57fe6d", 'major': 1, 'minor': 2, 'x':100, 'y': 3, 'z':0},
+{'uuid': "b9407f30f5f8466eaff925556b57fe6d", 'major': 1, 'minor': 3, 'x':100, 'y': 67, 'z':0},
+{'uuid': "b9407f30f5f8466eaff925556b57fe6d", 'major': 1, 'minor': 5, 'x':50, 'y': 50, 'z':0}]
 
 beacons = {}
 for i in input_list:
--- Scripts/localization.py
+++ Scripts/localization.py
@@ -22,46 +22,65 @@ min_diff_anchors = math.ceil(len(beacons)*min_diff_anchors_ratio)
 
 
 #count how many different beacons have sent the signal
-'''
-def count_n_diff_anchors(distances):
-    minors = []
-    for d in distances:
-        if d.minor not in minors:
-            minors.append(d.minor)
-    return len(minors)
-'''
+def count_n_diff_anchors(measures_batch):
+    diff_anchors = []
+    for d in measures_batch:
+        if d['minor'] not in diff_anchors:
+            diff_anchors.append(d['minor'])
+    return len(diff_anchors)
+
 
 #construct key
 def get_id(beacon):
     return beacon['uuid'] + ':' + str(beacon['major']) + ':' + str(beacon['minor'])
 
+#Trilateration
+def trilateration(data):
+    print 'TRILATERATION'
+    A = np.empty((0,2))
+    b = np.empty((0,1))
+    _id = get_id(data[-1])      #get _id of the last measure
+    last_anchor = beacons[_id]  #get last anchor
+    last_dist = data[-1]['dist']
+    for d in data[:-1]:
+        _id = get_id(d)
+        anchor = beacons[_id] #get anchor
+        A = np.append(A, [[ (last_anchor.x - anchor.x) , (last_anchor.y - anchor.y) ]], axis = 0)
+        b_i = ((d['dist'] ** 2) - (last_dist ** 2)) - ((anchor.x ** 2) - (last_anchor.x **2)) - ((anchor.y ** 2) - (last_anchor.y **2))
+        b = np.append(b, [[b_i]], axis=0)
+
+    ls = np.linalg.lstsq(2*A,b, rcond=None)
+    pos = ls[0]
+    print pos
+    print 'END TRILATERATION'
+    return A
+
 
 avg_time = 0
 while True:
-    diff_available_anchors = []
+    measures_batch = []
     ts_milli = time.time() * 1000
     data = ast.literal_eval(udp_dao.readData())
     _id = get_id(data)
     if beacons.has_key(_id):
         print str(_id)
-        dist = round(10.0 ** (( TxPower - data['rssi'] )/(10.0 * alpha)), decimal_approximation)
+        dist = round(10.0 ** (( TxPower - data['rssi'] )/(10.0 * alpha)), decimal_approximation)    #compute distance between device and beacon
+        data['dist'] = dist #add to the data dictionary the distance
         meas = b.Measure(data['rssi'], dist, data['timestamp'])
-        beacons[_id].measures.append(meas)
-        diff_available_anchors.append(_id)
-        print  'minor: ', beacons[_id].minor, 'dist: ', beacons[_id].measures[-1].distance, '#measures: ', len(beacons[_id].measures)
-        while (data['timestamp'] - ts_milli) < data_interval or len(diff_available_anchors) < min_diff_anchors:
+        beacons[_id].measures.append(meas)  #store measurement in the beacon class
+        measures_batch.append(data)
+        #print  'minor: ', beacons[_id].minor, 'dist: ', beacons[_id].measures[-1].distance, '#measures: ', len(beacons[_id].measures)
+        while (data['timestamp'] - ts_milli) < data_interval or count_n_diff_anchors(measures_batch) < min_diff_anchors:
             data = ast.literal_eval(udp_dao.readData())
             _id = get_id(data)
-            if beacons.has_key(get_id(data)):
+            if beacons.has_key(_id):
                 print str(_id)
-                dist = round(10.0 ** (( TxPower - data['rssi'] )/(10.0 * alpha)), decimal_approximation)
+                dist = round(10.0 ** (( TxPower - data['rssi'] )/(10.0 * alpha)), decimal_approximation)    #compute distance between device and beacon
+                data['dist'] = dist #add to the data dictionary the distance
                 meas = b.Measure(data['rssi'], dist, data['timestamp'])
-                beacons[_id].measures.append(meas)
-                diff_available_anchors.append(_id)
-                print  'minor: ', beacons[_id].minor, 'dist: ', beacons[_id].measures[-1].distance, '#measures: ', len(beacons[_id].measures)
-        print 'diff_available_anchors: ', len(diff_available_anchors)
+                beacons[_id].measures.append(meas)  #store measurement in the beacon class
+                measures_batch.append(data)
+                #print 'measures_batch[-1][dist]: ', measures_batch[-1]['dist'], 'minor', measures_batch[-1]['minor']
+        print 'measures_batch length: ', len(measures_batch)
         end_time = time.time() * 1000
-    #TODO ADD LOCALIZATION
-    #remove all measurements for each beacon
-    for _id in diff_available_anchors:
-        beacons[_id].measures = []
+        trilateration(measures_batch)

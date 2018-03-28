1ba1940fe458a8cec0d897b5e0ed42edc3d0d9b4
Unknown <umbertofazio92@gmail.com>
2018-03-28 09:40:54 +0200

    fill mongo fingerprinting update; get_id() in measure

--- Code/fill_fingerprinting_mongo.py
+++ Code/fill_fingerprinting_mongo.py
@@ -5,33 +5,16 @@ import anchors_config as ac
 import where_is_berry as wib
 import time
 import DAO
+import measure
 
 min_data_per_anchor = 5
-
-# check if for each anchor there are enogh data
-def ok_counts(counts):
-    ok = True
-    for c in counts:
-        if counts[c] < min_data_per_anchor:
-            ok = False
-            break
-    return ok
 
 #anchors
 anc = ac.getAnchors()
 anchors = anc['anchors']
-print [anchors[a].getID() for a in anchors]
-anchors_ids = anc['anchors_ids']
 anchor_id_keys = anc['idKeys']
-
-#construct key
-def get_id(data):
-    _id = ''
-    for i in anchor_id_keys[:-1]:
-        _id += str(data[i]) + ':'
-    _id += str(data[anchor_id_keys[-1]])
-    return _id
-
+print 'ANCHORS:'
+print [anchors[a].getID() for a in anchors]
 
 #get mongo collection
 mongo = MongoClient()
@@ -46,15 +29,17 @@ db = mongo.fingerprinting   # db
 models = db.models  # collection of models of different rooms
 map = db.map_uni    # collection
 
-
 # fill model
 ts = time.time()
 model_anchors = {'location': 'uni', 'anchors': {}, 'timestamp': ts}
+# delete old models if this room is already in the collection
+daletion = db.models.delete_many({'location':'uni'})
+
+#insert new anchors config
 for a in anchors:
     model_anchors['anchors'][anchors[a].getID()] = anchors[a].coordinates
 
 models.insert(model_anchors)
-
 
 # initialize dictionary of counts
 counts = {}
@@ -64,13 +49,14 @@ for a in anchors:
 #get data form udp
 dao = DAO.UDP_DAO("localhost", 12346)
 
-pi_pos = {'x':0.5,'y':0.5,'z':0}
-while not ok_counts(counts):    #TODO
+pi_pos = {'x':0.5,'y':0.5,'z':0}    #position of the device in the map
+#from each anchor store 'min_data_per_anchor' rssi
+while False in [counts[c] >= min_data_per_anchor for c in counts]:
     data = ast.literal_eval(dao.readData())
-    _id = get_id(data)
+    _id = measure.get_id(data,anchor_id_keys)
     while not anchors.has_key(_id):    #go on only if the first is a good anchor
         data = ast.literal_eval(dao.readData())
-        _id = get_id(data)
+        _id = measure.get_id(data, anchor_id_keys)
     doc = {'id': _id, 'timestamp': data['timestamp'], 'rssi': data['rssi'], 'coords': pi_pos}
     map.insert(doc)
     counts[_id] += 1
